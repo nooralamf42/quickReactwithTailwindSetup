ed569486fbbe2383d2c26d423c6abb3d318c6b63
nooralamf42 <nooralamf42@gmail.com>
2023-12-23 18:21:15 +0530

    quick react + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex h-screen justify-center items-center'>Quick React Setup with Tailwind</div>
+  )
+}
+
+export default App
